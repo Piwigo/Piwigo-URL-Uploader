ab8e6f54de18e414cd368ffd71ff458e011b30a4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-05 10:11:13 +0000

    [url_uploader] Add es_ES, thanks to : jpr928
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@19829 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add links'] = 'Añadir enlaces';
+$lang['Delete this item'] = 'Eliminar este artículo';
+$lang['File URL'] = 'URL del archivo';
+$lang['File URL is empty'] = 'URL del archivo está vacío';
+$lang['Invalid file URL'] = 'URL de archivo no válido';
+$lang['Invalid file type'] = 'Tipo de archivo no válido';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Cargador multiple no funciona? Prueba el <a href="%s">solo carga </ a>.';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Un enlace por línea, nombre de la foto y  url separada con una «|». Nombre de la foto es opcional.';
+$lang['Pending'] = 'Pendiente';
+$lang['Unable to download file'] = 'No se puede descargar el archivo';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = '¿Quieres subir muchos archivos? Pruebe el cargador <a href="%s"> cargador múltiple </ a>.';
+?>
