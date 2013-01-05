8c1cf1c98a87c2fba988087d2dc19a36e58d0181
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-05 18:22:20 +0000

    [url_uploader] Add it_IT, thanks to : Ericnet
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@19867 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['File URL'] = 'File URL';
+$lang['File URL is empty'] = 'URL del file è vuoto';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Vuoi caricare molti file? Prova il <a href="%s">uploader multiplo</a> invece.';
+$lang['Unable to download file'] = 'Impossibile scaricare il file';
+$lang['Pending'] = 'In attesa';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Un link per riga, nome foto separata e url con un &laquo; | &raquo;. Il nome della Photo è facoltativo.';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Uploader multiplo non funzionante? Prova il <a href="%s">singolo uploader</a> invece.';
+$lang['Invalid file type'] = 'Tipo di file non valido';
+$lang['Invalid file URL'] = 'File URL non valido';
+$lang['Add links'] = 'Aggiungi links';
+$lang['Delete this item'] = 'Elimina questo articolo';
+?>
