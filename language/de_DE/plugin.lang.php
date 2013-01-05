5ecf6898a7b70557612398d3b1f5836246d1ba02
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-05 16:09:47 +0000

    [url_uploader] Add de_DE, thanks to : Yogie
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@19856 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add links'] = 'Füge einen Verweis hinzu';
+$lang['Delete this item'] = 'Lösche dieses Element';
+$lang['File URL'] = 'Datei URL';
+$lang['File URL is empty'] = 'Datei URL ist leer';
+$lang['Invalid file URL'] = 'Ungültige Datei URL';
+$lang['Invalid file type'] = 'Ungültige Dateierweiterung';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Der Mehrfach-Upload funktioniert nicht? Versuche den <a href="%s">Einzel-Upload</a>.';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Einen Verweis pro Zeile, grenze Foto-Name und URL mit einem &laquo; | &raquo; ab. Der Foto-Name ist optional.';
+$lang['Pending'] = 'Ausstehend';
+$lang['Unable to download file'] = 'Unmöglich die Datei herunter zu laden';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Mehrere Dateien gleichzeitig hochladen? Versuche den <a href="%s">Mehrfach-Upload</a>.';
+?>
