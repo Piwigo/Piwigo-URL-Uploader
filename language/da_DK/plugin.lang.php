f94d1f8f0747c975d5f2c0f7bd7d34a8da318357
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-05 08:12:27 +0000

    [url_uploader] Add da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@19827 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add links'] = 'Tilføj links';
+$lang['Delete this item'] = 'Slet denne forekomst';
+$lang['File URL'] = 'Fil-URL';
+$lang['File URL is empty'] = 'Fil-URL er tom';
+$lang['Invalid file URL'] = 'Ugyldig fil-URL';
+$lang['Invalid file type'] = 'Ugyldig filtype';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Masseupload fungerer ikke? Prøv <a href="%s">enkeltupload</a> i stedet.';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Et link pr. linje, adskil fotografinavn og -URL med en &laquo; | &raquo;. Fotografinavn er valgfrit.';
+$lang['Pending'] = 'Afventer';
+$lang['Unable to download file'] = 'Kunne ikke downloade filen';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Ønsker du at uploade mange filer? Prøv <a href="%s">masseupload</a> i stedet.';
+?>
