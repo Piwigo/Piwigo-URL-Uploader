9c6a2283ef67df3988e76e7d61e81758cb9e5695
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-07 05:38:09 +0000

    [url_uploader] Add pt_PT, thanks to : lola
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@19928 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add links'] = 'Adicionar links';
+$lang['Delete this item'] = 'Apagar este artigo';
+$lang['File URL'] = 'Arquivo URL';
+$lang['File URL is empty'] = 'O arquivo URL está vazio';
+$lang['Invalid file URL'] = 'Arquivo URL inválido';
+$lang['Invalid file type'] = 'Tipo de arquivo inválido';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Multiplo carregamento não funciona? tente o <a href="%s">carregamento individual</a>';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Um link por linha, separar o nome da foto e a URL com um &laquo; | &raquo;. O nome da foto é opcional ';
+$lang['Pending'] = 'Pendente';
+$lang['Unable to download file'] = 'Incapaz de descarregar o arquivo';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Deseja carregar muitos arquivos? Tente o <a href="%s">carregamento multiplo</a> instead.';
+?>
