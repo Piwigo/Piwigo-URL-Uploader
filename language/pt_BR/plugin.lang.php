7f046788e5ca04543dd7cebae0a007b125de2c2f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-07 17:49:05 +0000

    [url_uploader] Add pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@19943 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add links'] = 'Adicione Links';
+$lang['Delete this item'] = 'Apague esse item';
+$lang['File URL'] = 'URL do arquivo';
+$lang['File URL is empty'] = 'URL do arquivo está vazia';
+$lang['Invalid file URL'] = 'URL do arquivo inválida';
+$lang['Invalid file type'] = 'Tipo de arquivo inválido';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Múltiplos envio não funciona? Tente então o <a href="%s">envio indiviudal</a>';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Um link por linha, separe o nome da foto e a url com um &laquo; | &raquo;.';
+$lang['Pending'] = 'Pendente';
+$lang['Unable to download file'] = 'Não foi possível baixar o arquivo';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Quer enviar vários arquivos? Tente então o <a href="%s">envio múltiplo</a>';
+?>
