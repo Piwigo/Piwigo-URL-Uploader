9b4979dda64973a5de1449375cce1f3a13e9320d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-08 08:34:37 +0000

    [url_uploader] Add el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@19982 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Θέλετε να ανεβάσετε πολλά αρχεία; Δοκιμάστε το <a href="%s">multiple uploader</a>.';
+$lang['Unable to download file'] = 'Δεν είναι δυνατή η λήψη αρχείων';
+$lang['Pending'] = 'Εκκρεμής';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Μια σύνδεση ανά γραμμή, ξεχωρίστε το όνομα και το url της φωτογραφίας με ένα  &laquo; | &raquo;. Το Όνομα Φωτογραφίας είναι προαιρετικό.';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Πολλαπλός uploader δεν λειτουργεί; Δοκιμάστε το <a href="%s">single uploader</a>.';
+$lang['Invalid file type'] = 'Άκυρος τύπος αρχείου';
+$lang['Invalid file URL'] = 'Άκυρο URL αρχείου';
+$lang['File URL is empty'] = 'URL αρχείου είναι κενό';
+$lang['File URL'] = 'URL αρχείου';
+$lang['Delete this item'] = 'Διαγραφή αυτού του στοιχείου';
+$lang['Add links'] = 'Προσθέστε συνδέσμους';
+?>
