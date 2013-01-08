88cac37631adc1fa75960edb7a29b4d4c2d40a26
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-08 06:09:33 +0000

    [url_uploader] Add uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@19970 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Багаторазовий завантажувач не працює?Спробуйте <a href="%s">одноразовий завантажувач</a> замість цього.';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Бажаєте завантажити багато файлів? Спробуйте <a href="%s">багаторазовий завантажувач</a> замість цього.';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Одне посилання в лінію з окремим ім\'ям фото і url з &laquo; | &raquo;. Ім\'я фото необов\'язкове.';
+$lang['Add links'] = 'Додати посилання';
+$lang['Delete this item'] = 'Видалити цей елемент';
+$lang['File URL'] = 'Файл URL';
+$lang['File URL is empty'] = 'Файл URL порожній';
+$lang['Invalid file URL'] = 'Невірний URL файлу';
+$lang['Invalid file type'] = 'Невірний тип файлу';
+$lang['Pending'] = 'Очікування';
+$lang['Unable to download file'] = 'Не вдалося завантажити файл';
+?>
