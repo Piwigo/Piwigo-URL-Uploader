98ac4a9ca529504efca5cbc5f37146367610b570
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-08 20:02:54 +0000

    [url_uploader] Add tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@20002 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Satır başına bir bağlantı, fotoğraf adını ve URL konumunu &laquo; | &raquo; ile ayırın. Fotoğraf adı opsiyoneldir.';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Pek çok dosya mı yüklemek istiyorsunuz? Bunun yerine <a href="%s">Çoklu Yükleme</a> \'yi deneyebilirsiniz.';
+$lang['Unable to download file'] = 'Dosya indirilemedi';
+$lang['Pending'] = 'Bekliyor';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Çoklu yükleme çalışmıyor? Bunun yerine <a href="%s">Tekil Yükleme</a> \'yi deneyin.';
+$lang['Invalid file type'] = 'Geçersiz dosya tipi';
+$lang['Invalid file URL'] = 'Dosya URL\'si geçersiz';
+$lang['File URL is empty'] = 'Dosya URL alanı boş';
+$lang['Delete this item'] = 'Bunu sil';
+$lang['File URL'] = 'Dosya URL';
+$lang['Add links'] = 'Bağlantıları ekle';
+?>
