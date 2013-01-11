56f68ecd5528566c705387d53d9be89936d9e195
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-11 07:30:42 +0000

    [url_uploader] Add lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@20060 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Vai vēlaties augšupielādēt daudz failus? Izmēģiniet <a href="%s"> daudzfailu augšupielādētāju </ a>.';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Viena saite rindiņā, atdaliet foto nosaukumu un URL ar &laquo; | &raquo;.  Foto nosaukums nav obligāts.';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Vairāki augšupielādētāji nedarbojas? Tā vietā izmēģiniet <a href="%s"> augšupielādētāju </ a>.';
+$lang['Unable to download file'] = 'Nav iespējams lejupielādēt failu';
+$lang['Pending'] = 'Gaidīšana';
+$lang['Invalid file type'] = 'Kļūdains faila tips';
+$lang['Invalid file URL'] = 'Kļūdains faila URL';
+$lang['File URL is empty'] = 'Faila URL ir tukšs';
+$lang['File URL'] = 'Faila URL';
+$lang['Delete this item'] = 'Dzēst šo vienumu';
+$lang['Add links'] = 'Pievienot saites';
+?>
