74dc1579242a8175c85e26f9abad30f39929951f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-14 12:03:09 +0000

    [url_uploader] Add bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@20128 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Искате да качите много файлове. Пробвайте  <a href="%s"> Мулти ъплоудера </a> тогава.';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'По един линк на ред, разделени име и адрес на снимка с &laquo; | &raquo;.  Името на снимката е опционално.';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Мулти ъплоудера не работи. Пробвайте с <a href="%s"> Обикновенния ъплоудер</a> тогава.';
+$lang['Pending'] = 'За одобрение';
+$lang['Unable to download file'] = 'Файла не може да бъде свален';
+$lang['Invalid file type'] = 'Невалиден файлов тип';
+$lang['Invalid file URL'] = 'Грешен файл адрес';
+$lang['File URL is empty'] = 'Невъведен адрес на файл';
+$lang['File URL'] = 'Адрес на файл';
+$lang['Delete this item'] = 'Изтриване на позицията';
+$lang['Add links'] = 'Добавяне на линк';
+?>
