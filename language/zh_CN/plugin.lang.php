cd67193710ef6970fbb80300d327dd7aa544d49d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-24 20:43:09 +0000

    [url_uploader] Add zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@20365 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add links'] = '添加链接';
+$lang['Delete this item'] = '删除此项';
+$lang['File URL'] = '文件URL';
+$lang['File URL is empty'] = '文件URL为空';
+$lang['Invalid file URL'] = '文件URL无效';
+$lang['Invalid file type'] = '文件类型无效';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = '无法批量上传？试一下 <a href="%s">单个上传</a>。';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = '每行一个链接，以 &laquo; | &raquo; 分隔图片名称与URL。图片名称非必填。';
+$lang['Pending'] = '处理中';
+$lang['Unable to download file'] = '无法下载文件';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = '需要上传大量文件？请尝试 <a href="%s">批量上传</a>。';
+?>
