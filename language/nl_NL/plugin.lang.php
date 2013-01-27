ca0e89f9c86a66de5c5b1c2fc3b2a5d83ca474a0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-27 12:49:23 +0000

    [url_uploader] Update nl_NL, thanks to : Kees Hessels
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@20419 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -20,15 +20,16 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Werkt de meervoudige uploader niet? Probeer dan de <a href="%s">eenvoudige uploader</a>. ';
-$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Probeer de <a href="%s">meervoudige uploader</a> als je meer dan 1 bestand wil uploaden.';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Werkt de meervoudige uploader niet? Probeer dan de <a href="%s">enkelvoudige uploader</a>. ';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Probeer de <a href="%s">meervoudige uploader</a> als u meer dan één bestand wilt uploaden.';
 $lang['Add links'] = 'Links toevoegen';
-$lang['Delete this item'] = 'Verwijder dit deel';
-$lang['File URL'] = 'Bestands URL';
-$lang['File URL is empty'] = 'Bestands URL is leeg';
-$lang['Invalid file URL'] = 'Ongeldig bestands URL';
-$lang['Invalid file type'] = 'Ongeldig bestands type';
-$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = '1 link per regel, plaats een &laquo; | &raquo tussen de fotonaam en URL; Een fotonaam is niet verplicht.';
-$lang['Pending'] = 'In behandeling';
+$lang['Delete this item'] = 'Verwijder dit onderdeel';
+$lang['File URL'] = 'Bestands-URL';
+$lang['File URL is empty'] = 'Bestands-URL is leeg';
+$lang['Invalid file URL'] = 'Ongeldige bestands-URL';
+$lang['Invalid file type'] = 'Ongeldig bestandstype';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Eén link per regel, plaats een &laquo; | &raquo tussen de afbeeldingsnaam en URL; Een afbeeldingsnaam is niet verplicht.';
+$lang['Pending'] = 'Bezig';
 $lang['Unable to download file'] = 'Kan bestand niet downloaden';
+$lang['Add website URL in photo description'] = 'Voeg de website-URL toe aan de afbeeldingsomschrijving';
 ?>
