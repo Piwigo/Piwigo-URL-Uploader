7a91e952ca3820e331d0644a625e485c700dd44b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-02 15:34:59 +0000

    [url_uploader] Add sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@20523 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Chcete nahrať viac súborov? Skúste <a href="%s">viacnásobný nahrávač</a>.';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Viacnásobné nahrávanie nefunguje? Skúste <a href="%s">jednoduchý nahrávač</a>.';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Jeden link na riadok, osobitne názov fotky a url s &laquo; | &raquo;. Názov fotky je voliteľný.';
+$lang['Unable to download file'] = 'Nie je možné stiahnuť súbor';
+$lang['Pending'] = 'Čaká';
+$lang['Invalid file type'] = 'Chybný typ súboru';
+$lang['Invalid file URL'] = 'Chybné URL súboru';
+$lang['File URL is empty'] = 'URL súboru je prázdna';
+$lang['File URL'] = 'URL súboru';
+$lang['Delete this item'] = 'Vymazať položku';
+$lang['Add website URL in photo description'] = 'Pridať URL stránkyv popise fotky';
+$lang['Add links'] = 'Pridať linky';
+?>
