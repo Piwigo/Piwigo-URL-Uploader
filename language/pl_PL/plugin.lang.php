a639067b18e0ba5bbea5dfdf7500a6eb4df62eaf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-11 20:05:42 +0000

    [url_uploader] Add pl_PL, thanks to : K.S.
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@20683 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add links'] = 'Dodaj odnośniki';
+$lang['Add website URL in photo description'] = 'Dodaj adres strony w opisie zdjęcia';
+$lang['Delete this item'] = 'Usuń tę pozycję';
+$lang['File URL'] = 'Adres pliku';
+$lang['File URL is empty'] = 'Adres pliku jest pusty';
+$lang['Invalid file URL'] = 'Nieprawidłowy adres';
+$lang['Invalid file type'] = 'Nieprawidłowy typ pliku';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Multiple uploader nie działa? Wypróbuj <a href="%s">single uploader</a>.';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Jeden odnośnik w linii, oddziel nazwę zdjęcia i adres za pomocą &laquo; | &raquo;. Nazwa zdjęcia jest opcjonalna.';
+$lang['Pending'] = 'Oczekujące';
+$lang['Unable to download file'] = 'Nie można pobrać pliku';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Chcesz wgrać wiele plików? Wypróbuj <a href="%s">multiple uploader</a>.';
+?>
