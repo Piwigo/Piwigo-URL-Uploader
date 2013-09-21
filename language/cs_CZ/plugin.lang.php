a34db011f160ca0d8dd4967c541a0c5aa23dec4a
ddtddt <ddtddt@piwigo.org>
2013-09-21 06:35:47 +0000

    [extensions] - url_uploader -Add cs_CZ - Česky – Czech - Thanks to webprostor
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@24568 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['Invalid file URL'] = 'Neplatný soubor URL';
+$lang['Invalid file type'] = 'Neplatný typ souboru';
+$lang['File URL is empty'] = 'Soubor URL je prázdný';
+$lang['Unable to download file'] = 'Nelze stáhnout soubor';
+$lang['Pending'] = 'Pracuji';
+$lang['Delete this item'] = 'Odstranit tuto položku';
+$lang['File URL'] = 'URL souboru';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Chcete vložit více souborů? Zkuste <a href="%s">multi uploader</a>.';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Multi uploader nefunguje? Zkuste tedy <a href="%s">single uploader</a>.';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Jeden url odkaz na řádek, oddělte název fotky a url pomocí &laquo; | &raquo;. Název fotky není povinný.';
+$lang['Add links'] = 'Vložit odkazy';
+$lang['Add website URL in photo description'] = 'Vložit URL adresu webu do popisu fotografií';
+
+?>
