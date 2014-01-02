47ec7dec487ea18d1bde4e00e7f2e63c0c9f1c20
ddtddt <ddtddt@piwigo.org>
2014-01-02 15:47:48 +0000

    [extensions] - url_uploader - add fr_CA - Quebec - Québécois Thanks to TiOger
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@26403 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['Invalid file URL'] = 'URL du fichier invalide';
+$lang['Invalid file type'] = 'Type de fichier invalide';
+$lang['File URL is empty'] = 'L\'URL du fichier est vide';
+$lang['Unable to download file'] = 'Impossible de télécharger le fichier';
+$lang['Pending'] = 'En attente';
+$lang['Delete this item'] = 'Supprimer ce lien';
+$lang['File URL'] = 'URL du fichier';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Vous voulez télécharger plus de fichiers ? Essayez le <a href="%s">formulaire multiple</a>.';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Le formulaire multiple ne fonctionne pas ? Essayez le <a href="%s">formulaire simple</a>.';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Un seul lien par ligne, séparez le nom et l\'URL par un &laquo; | &raquo;. Le nom de la photo est optionel.';
+$lang['Add links'] = 'Ajouter les liens';
+$lang['Add website URL in photo description'] = 'Ajouter l\'URL du site web dans la description de la photo';
+
+?>
