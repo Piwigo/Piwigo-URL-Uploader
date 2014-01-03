cf1dbc693e9b40b9882bc61629285223644fbdf1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-03 18:33:38 +0000

    [url_uploader] Update eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@26412 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Ĉu vi volas alŝuti plurajn dosierojn? Alie provu la <a href="%s">pluropan elŝutilon</a>.';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Unu ligilo sur ĉiu linio, disigu la fotonomon kaj la fotoadreson per la signo &laquo; | &raquo;. Fotonomo estas opcia.';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Ĉu la pluropan elŝutilon ne funkcias? Alie provu la <a href="%s">unuopan elŝutilon</a>.';
+$lang['File URL'] = 'Dosieradreso';
+$lang['File URL is empty'] = 'Dosieradreso malplenas';
+$lang['Unable to download file'] = 'Neeblas elŝuti dosieron';
+$lang['Add links'] = 'Aldoni ligilojn';
+$lang['Add website URL in photo description'] = 'Aldoni retadreson en fotopriskribon';
+$lang['Delete this item'] = 'Forigi tiun eron';
+$lang['Invalid file URL'] = 'Nevalida dosieradreso';
+$lang['Invalid file type'] = 'Nevalida dosierotipo';
+$lang['Pending'] = 'Okazanta';
+?>
