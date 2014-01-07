eba58d0e43a9b7f1af6d616694f412b4395bfc54
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-07 21:37:44 +0000

    [url_uploader] Update fa_IR, thanks to : ipic
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@26493 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add links'] = 'افزودن پیوند ';
+$lang['Add website URL in photo description'] = 'افزودن پیوند از سایت دیگر  برای تصاویر ';
+$lang['Delete this item'] = 'حذف این مورد ';
+$lang['File URL'] = 'ادرس پیوند ';
+$lang['File URL is empty'] = 'فایل در پیوند خالی است ';
+$lang['Invalid file URL'] = 'پیوند نامعتبر است :(';
+$lang['Invalid file type'] = 'نوع فایل نامعتبراست';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = ' <a href="%s"> اپلودتکی</a> اپلود چندتایی کار نمیکند؟این را امتحان کنید ';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'هر خط یک لینک   &laquo; | &raquo;. با جدا کننده نام تصایو را جدا کنید 
+بین خودمون بمونه  نام عکس دلبخواهی است می توانید نزاریدش ';
+$lang['Pending'] = 'منتظرم';
+$lang['Unable to download file'] = 'فایل دانلود نشد';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'اپلود بیشتر میخواهید ؟ ایو امتحان کنید<a href="%s"> اپلود چند خطه</a> ';
+?>
