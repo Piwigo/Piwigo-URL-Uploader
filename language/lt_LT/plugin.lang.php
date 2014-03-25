68d2cc1c1134f853695238a0dc04f57d5de9a261
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-03-25 17:22:48 +0000

    [url_uploader] Update lt_LT, thanks to : vilisas
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@27939 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lt_LT/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add links'] = 'Pridėti nuorodas';
+$lang['Add website URL in photo description'] = 'Pridėti puslapio nuorodą į nuotraukos aprašymą';
+$lang['Delete this item'] = 'Pašalinti šitą';
+$lang['File URL'] = 'Failo nuoroda';
+$lang['File URL is empty'] = 'Failo nuoroda tuščia';
+$lang['Invalid file URL'] = 'Neteisinga failo nuoroda';
+$lang['Invalid file type'] = 'Neteisingas failo tipas';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Multi įkėlimas neveikia ? Pamėginkite <a href="%s">įkėlimą po vieną</a>';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Viena nuoroda į eilutę, atskirkite nuotraukos pavadinimą ir nuoroda su &laquo; | &raquo;. Nuotraukos pavadinimas neprivalomas.';
+$lang['Pending'] = 'Laukiama';
+$lang['Unable to download file'] = 'Neįmanoma parsisiųsti failo';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Norite įkelti daug failų ? Pamėginkite <a href="%s">multi įkėlimą.</a>';
