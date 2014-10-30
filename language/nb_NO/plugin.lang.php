b0c9cc804ae8fb23f958df98cf8619ebd011f94c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-30 12:33:24 +0000

    [url_uploader] Add nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/url_uploader@30336 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add website URL in photo description'] = 'Legge til nettsidens URL i bilde beskrivelsen';
+$lang['Delete this item'] = 'Slett dette elementet';
+$lang['File URL'] = 'Fil URL';
+$lang['File URL is empty'] = 'Fil URL er tom';
+$lang['Invalid file URL'] = 'Ugyldig fil URL';
+$lang['Invalid file type'] = 'Ugyldig fil type';
+$lang['Multiple uploader doesn\'t work? Try the <a href="%s">single uploader</a> instead.'] = 'Multiple opplasteren virker ikke? Prøv <a href="%s"> singel opplaster</a> i stedet.';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'En kobling per linje, separer bilde navn og url med en &laquo; | & Raquo;. Bilde navn er valgfritt.';
+$lang['Pending'] = 'Avventer';
+$lang['Unable to download file'] = 'Kan ikke laste ned filen';
+$lang['Want to upload many files? Try the <a href="%s">multiple uploader</a> instead.'] = 'Vil du laste opp mange filer? Prøv <a href="%s"> multiple opplasteren</a> i stedet.';
+$lang['Add links'] = 'Legge til linker';
