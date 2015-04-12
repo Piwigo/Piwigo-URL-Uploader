79208f8930ff333f5b4319269c9eb16d7604b50b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-04-12 12:03:30 +0200

    Add ca_ES, thanks to josep.torne

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add website URL in photo description'] = 'Afegeix una URL del lloc web en la descripció de la foto';
+$lang['Add links'] = 'Afegeix enllaços';
+$lang['Delete this item'] = 'Elimina aquest ítem';
+$lang['File URL'] = 'URL del fitxer';
+$lang['File URL is empty'] = 'La URL del fitxer és buida';
+$lang['Invalid file URL'] = 'URL no vàlida';
+$lang['Invalid file type'] = 'Tipus de fitxer no vàlid';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Un enllaç cada vegada, separeu el nom de la foto i la seva URL per un &laquo; | &raquo;. El nom de la foto és opcional.';
+$lang['Pending'] = 'Pendent';
+$lang['Unable to download file'] = 'No es pot descarregar el fitxer';
