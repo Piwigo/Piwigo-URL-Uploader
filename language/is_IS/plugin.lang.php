4e98893b14b030301b58147c71186e2f198d6686
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-04 18:43:52 +0100

    Update is_IS, thanks to gudjonsi

--- language/is_IS/plugin.lang.php
+++ language/is_IS/plugin.lang.php
@@ -6,3 +6,12 @@
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
 $lang['Add links'] = 'Bæta við krækju';
+$lang['Add website URL in photo description'] = 'Bættu við vefslóð við myndlýsingu';
+$lang['Delete this item'] = 'Eyða þessu atriði';
+$lang['File URL'] = 'Vefslóð skráar';
+$lang['File URL is empty'] = 'Vefslóð skráar er auð';
+$lang['Invalid file URL'] = 'Ógild vefslóð skráar';
+$lang['Invalid file type'] = 'Ógild skráar tegund';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'Einn hlekkur fyrir línu, aðskiljið heiti myndar og vefslóð með &laquo; | &raquo;. Heiti myndar er valfrjálst.';
+$lang['Pending'] = 'Í bið';
+$lang['Unable to download file'] = 'Getur ekki sótt skrána';
