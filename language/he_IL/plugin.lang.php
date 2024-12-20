9159e3c5d5a5b0ef03f18e427d781b7da6b934f6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 22:49:34 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Add links'] = 'הוסף קישורים';
+$lang['Add website URL in photo description'] = 'הוסף כתובת URL של אתר בתיאור התמונה';
+$lang['Delete this item'] = 'מחק פריט זה';
+$lang['File URL'] = 'כתובת URL של קובץ';
+$lang['File URL is empty'] = 'כתובת URL של קובץ ריקה';
+$lang['Invalid file URL'] = 'כתובת URL של קובץ לא חוקית';
+$lang['Invalid file type'] = 'סוג קובץ לא חוקי';
+$lang['One link by line, separate photo name and url with a &laquo; | &raquo;. Photo name is optional.'] = 'קישור אחד לכל שורה, הפרידו בין שם התמונה וה-URL באמצעות &laquo; | &raquo;. שם התמונה אופציונלי.';
+$lang['Pending'] = 'בהמתנה';
+$lang['Unable to download file'] = 'לא ניתן להוריד את הקובץ';
